291eae12241a8155bad0032178c575fb541db266
demurean <arielle.felicia@gmail.com>
2025-03-08 21:30:32 -0800

    changes to mentee and .env

--- models/Mentee.js
+++ models/Mentee.js
@@ -2,11 +2,12 @@ const mongoose = require("mongoose");
 
 const menteeSchema = new mongoose.Schema({
   name: String,
+  field: String,
   portrait: String,
   aboutMe: String,
   loginMail: { type: String, unique: true },
   loginPass: String, // Will be hashed before saving
-  subscriptionPlan: { type: String, enum: ["monthly", "yearly", "none"], default: "none" },
+  // subPlan: none
 });
 
 module.exports = mongoose.model("Mentee", menteeSchema);

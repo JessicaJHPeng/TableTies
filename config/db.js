32fcfdf86026c807ee3c039951d6fa8433b8a3ad
demurean <arielle.felicia@gmail.com>
2025-03-08 22:04:29 -0800

    figured out .env

--- config/db.js
+++ config/db.js
@@ -4,7 +4,9 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // Timeout after 5 seconds instead of 30 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     });
     console.log("MongoDB connected...");
   } catch (err) {
--- server.js
+++ server.js
@@ -1,49 +1,49 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const connectDB = require("./config/db");
-require("dotenv").config();
-
-const app = express();
-
-// Add functions to create a new project/mentor/user/whatever and add functions to edit/delete them
-// https://www.w3schools.com/nodejs/nodejs_mongodb.asp
-
-// Middleware
-app.use(cors());
-app.use(bodyParser.json());
-
-// Connect to MongoDB
-connectDB();
-
-// Routes
-app.use("/api/items", require("./routes/itemRoutes"));
-
-// Start Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-
-
-// require("dotenv").config();
 // const express = require("express");
+// const bodyParser = require("body-parser");
 // const cors = require("cors");
 // const connectDB = require("./config/db");
-
-// const menteeRoutes = require("./routes/menteeRoutes");
+// require("dotenv").config();
 
 // const app = express();
 
+// // Add functions to create a new project/mentor/user/whatever and add functions to edit/delete them
+// // https://www.w3schools.com/nodejs/nodejs_mongodb.asp
+
 // // Middleware
 // app.use(cors());
-// app.use(express.json());
+// app.use(bodyParser.json());
 
 // // Connect to MongoDB
 // connectDB();
 
 // // Routes
-// app.use("/api/mentees", menteeRoutes);
+// app.use("/api/items", require("./routes/itemRoutes"));
 
+// // Start Server
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./config/db");
+
+const menteeRoutes = require("./routes/menteeRoutes");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+connectDB();
+
+// Routes
+app.use("/api/mentees", menteeRoutes);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
